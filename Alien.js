5aaa0897ab60281fc06cd58cd1aa569c4482fd43
Saul <phasecross@hotmail.com>
2022-11-16 09:00:16 -0700

    Formatted, added p5js CDN, and fixed image paths

--- Alien.js
+++ Alien.js
@@ -1,41 +1,43 @@
-function Alien(x, y, dir) {
+class Alien {
+  constructor(x, y, dir) {
     this.x = x;
     this.y = y;
     this.r = 16;
     this.dir = dir;
     this.toDestroy = false;
 
-    this.show = function() {
-        noFill();
-        noStroke();
-        ellipse(this.x, this.y, this.r * 2, this.r * 2);
-        image(alien, this.x, this.y, 32, 32);
+    this.show = function () {
+      noFill();
+      noStroke();
+      ellipse(this.x, this.y, this.r * 2, this.r * 2);
+      image(alien, this.x, this.y, 32, 32);
     };
 
-    this.move = function() {
-        this.x += this.dir * 0.3;
+    this.move = function () {
+      this.x += this.dir * 0.3;
     };
 
-    this.bringCloser = function(times) {
-        this.y += 48 * times;
+    this.bringCloser = function (times) {
+      this.y += 48 * times;
     };
 
-    this.edge = function() {
-        if (this.x < 30) {
-            this.x = 30;
-            return true;
-        } else if (this.x > 390) {
-            this.x = 390;
-            return true;
-        }
-        return false;
+    this.edge = function () {
+      if (this.x < 30) {
+        this.x = 30;
+        return true;
+      } else if (this.x > 390) {
+        this.x = 390;
+        return true;
+      }
+      return false;
     };
 
-    this.destroy = function() {
-        this.toDestroy = true;
+    this.destroy = function () {
+      this.toDestroy = true;
     };
 
-    this.verticalLimit = function() {
-        return this.y > 460;
+    this.verticalLimit = function () {
+      return this.y > 460;
     };
+  }
 }
--- Bullet.js
+++ Bullet.js
@@ -1,31 +1,33 @@
-function Bullet(x, y) {
+class Bullet {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
     this.r = 8;
     this.toDestroy = false;
 
-    this.show = function() {
-        noFill();
-        noStroke();
-        ellipse(this.x, this.y, this.r * 2, this.r * 2);
-        image(bolt, this.x, this.y, 14, 14);
+    this.show = function () {
+      noFill();
+      noStroke();
+      ellipse(this.x, this.y, this.r * 2, this.r * 2);
+      image(bolt, this.x, this.y, 14, 14);
     };
 
-    this.move = function() {
-        this.y += -10;
+    this.move = function () {
+      this.y += -10;
     };
 
-    this.offScreen = function() {
-        return this.y <= -this.r;
+    this.offScreen = function () {
+      return this.y <= -this.r;
     };
 
-    this.hits = function(alien) {
-        let distance = dist(this.x, this.y, alien.x, alien.y);
+    this.hits = function (alien) {
+      let distance = dist(this.x, this.y, alien.x, alien.y);
 
-        return distance <= (this.r + alien.r);
+      return distance <= this.r + alien.r;
     };
 
-    this.destroy = function() {
-        this.toDestroy = true;
-    }
+    this.destroy = function () {
+      this.toDestroy = true;
+    };
+  }
 }
